a38774436f7c7989f5e81a2c01c2d778f103cee4
Priyansh Pandya <priyansh.imscit21@gmail.com>
2026-05-05 10:42:28 +0530

    Refactor navbar scroll effect and smooth scroll

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // NAVBAR SCROLL EFFECT
 window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
-  
-    if(window.scrollY > 50){
-      nav.style.background = "rgba(0,0,0,0.8)";
-    } else {
-      nav.style.background = "rgba(0,0,0,0.4)";
-    }
-  });
-  
-  // smooth scroll
+  const nav = document.querySelector(".navbar");
+
+  if(window.scrollY > 50){
+    nav.style.background = "rgba(0,0,0,0.8)";
+  } else {
+    nav.style.background = "rgba(0,0,0,0.4)";
+  }
+});
+
+// SMOOTH SCROLL
 document.querySelectorAll('a[href^="#"]').forEach(link=>{
-    link.addEventListener("click", e=>{
-      e.preventDefault();
-      document.querySelector(link.getAttribute("href"))
-        .scrollIntoView({behavior:"smooth"});
-    });
+  link.addEventListener("click", e=>{
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({behavior:"smooth"});
   });
+});
